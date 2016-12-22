e93f34388f7bff2e78a6602b1575abea1c03418b
huangxuewen <1337893145@qq.com>
2016-12-22 17:51:47 +0800

    开发

---
+++ src/main/java/com/bosssoft/platform/es/jdbc/helper/SearchConverter.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * $Header$
+ * $Revision$
+ * $Date$
+ *
+ *==============================================================================
+ *
+ * Copyright (c) 2001-2016 Bosssoft Co, Ltd.
+ * All rights reserved.
+ * 
+ * Created on 2016年12月22日
+ *******************************************************************************/
+
+
+package com.bosssoft.platform.es.jdbc.helper;
+
+/**
+ * TODO 此处填写 class 信息
+ *
+ * @author huangxuewen (mailto:huangxuewen@bosssoft.com.cn)
+ */
+/**
+ * TODO 此处填写 class 信息
+ *
+ * @author yourname (mailto:yourname@bosssoft.com.cn)
+ */
+/*
+ * 修改历史
+ * $Log$ 
+ */
+public class SearchConverter {
+
+}
+
+/*
+ * 修改历史
+ * $Log$ 
+ */
